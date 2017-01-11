70d76ac77773b7a6eaba67180b77d64f3ad0edde
Mauro <lavaramano@gmail.com>
2017-01-11 12:57:36 -0300

    Adding admin views for pattern models

--- pattern/admin.py
+++ pattern/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from models import (
+    Language,
+    Pattern,
+    Comment
+)
 
-# Register your models here.
+class LanguageAdmin(admin.ModelAdmin):
+    pass
+
+class PatternAdmin(admin.ModelAdmin):
+    pass
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Language, LanguageAdmin)
+admin.site.register(Pattern, PatternAdmin)
+admin.site.register(Comment, CommentAdmin)
