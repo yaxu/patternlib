ee82e06c03917cb003ca7aa21fcee09b18166e1c
alex <alex@slab.org>
2017-01-13 12:17:28 +0000

    aha

--- pattern/urls.py
+++ pattern/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^add/$', views.add, name='pattern_add'),
+    url(r'^(?P<pk>\d+)/$', views.pattern_detail, name='pattern_detail'),
     url(r'^logout/$', views.logout)
 ]
--- pattern/views.py
+++ pattern/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 from .models import *
 from .forms import PatternForm
+import json
 
 def index(request):
     context = {}
@@ -13,25 +14,31 @@ def index(request):
 def add(request):
     if request.method == "POST":
         form = PatternForm(request.POST)
-        pattern = form.save(commit=False)
-        username = request.user.username
-        language = Language.objects.get_or_create(
-            name = "TidalCycles"
-        );
-        pattern.language = language[0]
-        ident = Identity.objects.get_or_create(
-            user = request.user,
-            service = None,
-            defaults={'name': '',
-                      'ident': username
-            },
-        ),
-        pattern.author = ident[0][0]
-        pattern.save()
+        if form.is_valid():
+            pattern = form.save(commit=False)
+            username = request.user.username
+            language = Language.objects.get_or_create(
+                name = "TidalCycles"
+            );
+            pattern.language = language[0]
+            ident = Identity.objects.get_or_create(
+                user = request.user,
+                service = None,
+                defaults={'name': '',
+                          'ident': username
+                },
+            ),
+            pattern.author = ident[0][0]
+            pattern.save()
+            return redirect('pattern_detail', pk=pattern.pk)
     else:
         form = PatternForm()
     context = {'form': form}
     return render(request, 'pattern/add.html', context)
+
+def pattern_detail(request, pk):
+    pattern = get_object_or_404(Pattern, pk=pk)
+    return render(request, 'pattern/detail.html', {'pattern': pattern})
 
 def logout(request):
     auth_logout(request)
