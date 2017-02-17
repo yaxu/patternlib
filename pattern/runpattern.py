7f8d14f48e7756bf70566b5a9d7a82172fd9524c
alex <alex@slab.org>
2017-02-17 14:07:28 +0000

    switch to local storage rather than uploading to clyp

--- pattern/runpattern.py
+++ pattern/runpattern.py
@@ -6,27 +6,34 @@ from subprocess import Popen, PIPE, STDOUT
 
 from django.conf import settings
 
+def uniqid():
+    m = time.time()
+    uniqid = '%8x%05x' %(math.floor(m),(m-math.floor(m))*1000000)
+    return uniqid
+
 def render_and_upload(code):
     code = code.decode('unicode_escape').encode('ascii', 'ignore')
     response = ""
 
-    p = Popen([settings.RUNPATTERN_BIN],
-                  stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    fnid = uniqid()
+    fn = uniqid() + ".mp3"
+    path = e.sub(r"(..)", r'\1/', fnid)
+    fullpath = os.path.join(settings.BASE_DIR, "mp3s", path)
+    
+    try:
+        os.makedirs(fullpath)
+    except e:
+        if e.errno != errno.EEXIST:
+            raise
+        
+    abspath = os.path.join(fullpath, fn)
+
+    p = Popen([settings.RUNPATTERN_BIN, abspath],
+               stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     tidal = p.communicate(input=code)[0]
 
     if p.returncode == 0:
-        clyp_file_upload_url = 'http://upload.clyp.it/upload'
-        cyclefile = open(os.path.join(settings.RUNPATTERN_DIR,
-                                      'cycle.wav'), 'rb')
-        send_files = {
-            'audioFile': ('cycle.wav', cyclefile, 'audio/wav'),
-            'description': code
-        }
-        r = requests.post(clyp_file_upload_url, files=send_files)
-        if r.status_code == 200:
-            response = r.json()['Url']
-        else:
-            response = "Sorry there's a problem uploading the pattern"
+        response = os.path.join("/mp3s", path, fn)
     else:
         response = "Sorry there's something wrong with that pattern: {}"
 
