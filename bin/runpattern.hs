4bbf102a589075daaa7b033b6ef9b86dfda3f9c0
alex <alex@slab.org>
2017-05-24 14:01:04 +0100

    twiddles

--- bin/runpattern.hs
+++ bin/runpattern.hs
@@ -6,6 +6,7 @@ import Control.Concurrent
 import System.Cmd
 import Text.HTML.TagSoup.Entity (lookupEntity)
 import System.Posix.Resource
+import Sound.Tidal.Vis2
 
 unescapeEntities :: String -> String
 unescapeEntities [] = []
@@ -28,11 +29,9 @@ main = do a <- getArgs
           r <- runTidal $ unescapeEntities code
           respond fn r
    where respond fn (OK p)
-           = do (cps, nudger, getNow) <- cpsUtils'
-                -- (d,_) <- superDirtSetters getNow
+           = do vis fn $ dirtToColour (fast 12 p)
                 d <- dirtStream
                 system $ "ecasound -t:" ++ show (seconds+2) ++ " -i jack,dirt -o " ++ fn ++ " &"
-                -- system $ "ecasound -t:" ++ show (seconds+2) ++ " -i jack,SuperCollider -o " ++ fn ++ " &"
                 d p
                 threadDelay (seconds * 1000000)
                 exitSuccess
